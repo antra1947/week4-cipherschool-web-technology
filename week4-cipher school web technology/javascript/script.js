af60e490db9427816a0dddf4adfb76abaa017ef4
Antra  Gupta <115134630+antra1947@users.noreply.github.com>
2023-01-08 20:53:49 +0530

    Add files via upload

---
+++ week4-cipher school web technology/javascript/script.js
@@ -0,0 +1,23 @@
+// console.log("script start");
+
+// setTimeout(() =>{
+//     console.log("inside setTimeout");
+// },1000);
+
+// console.log("script end");
+
+const body = document.body;
+ const intervalId = setInterval(() => {
+    const red = Math.floor(Math.random() * 126);
+    const green = Math.floor(Math.random() * 126);
+    const blue = Math.floor(Math.random() * 126);
+    const rgb = 'rgb (${red},${green},${blue})';
+    body.style.background = rgb;
+},1000);
+
+
+
+
+
+
+
